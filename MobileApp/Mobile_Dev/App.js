a25aed160d4dcb0cb58edb8d61b2405f02904907
MustafaiSaleem <saleemraza1522@gmail.com>
2023-08-10 00:09:28 +0530

    Done with Login UI

--- MobileApp/Mobile_Dev/App.js
+++ MobileApp/Mobile_Dev/App.js
@@ -7,13 +7,11 @@ import Greeting from './components/Greeting';
 import Carousel from './components/Carousel';
 import Options from './components/Border';
 import BottomNavigation from './components/BottomBar';
-import LoginUI from './components/LoginUI'
-import LoginDetailUI from './components/LoginDetail'
 
 export default function App() {
   return (
     <>
-      {/* <ShakeFeature/> */}
+      <ShakeFeature/>
       <SafeAreaView style={styles.container}>
 
         <Header/>
@@ -27,15 +25,7 @@ export default function App() {
         <Options/>
 
         <BottomNavigation/>
-<<<<<<< Updated upstream
         
-=======
-
-        {/* <LoginUI/> */}
-
-        {/* <LoginDetailUI/> */}
-
->>>>>>> Stashed changes
         <StatusBar style="auto" />
 
       </SafeAreaView>
