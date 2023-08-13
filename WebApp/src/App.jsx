656ace06978fced756c0ea8d6805acb393af6baa
luvSainii <beasaini2002@gmail.com>
2023-08-13 20:59:09 +0530

    Adding header in WebApp

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -1,8 +1,10 @@
-
+import React from 'react';
+import Header from './components/Header'
 export default function App() {
   return (
     <div>
-      <h1>Trust</h1>
-      </div>
-  );
+      <Header/>
+    </div>
+  )
 }
+
