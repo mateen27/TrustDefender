8aa5ea4c8913a5801bafeac0408390a738544443
mateen27 <khanmateen2713@gmail.com>
2023-09-06 23:40:45 +0530

    Made Bottom Tab Navigator Functional

--- MobileApp/Mobile_Dev/components/BottomMain.js
+++ MobileApp/Mobile_Dev/components/BottomMain.js
@@ -1,27 +1,83 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Tab = createBottomTabNavigator();
 
-import Main from './Main';
-import Search from '../screens/Search';
-import Chat from '../screens/Chat';
-import About from '../screens/About';
+import Main from "./Main";
+import Search from "../screens/Search";
+import Chat from "../screens/Chat";
+import About from "../screens/About";
+
+// Home Icon & Chat Image
+import { Ionicons } from '@expo/vector-icons';
+//Search Icon & user Image  
+import { Feather } from '@expo/vector-icons';
 
 function BottomMain() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Main" component={Main} options={{
-          headerShown : false ,
-          tabBarActiveBackgroundColor : '#333',
-          tabBarInactiveBackgroundColor : '#333' , 
-          tabBarActiveTintColor : 'white' , 
-        }}/>
-      <Tab.Screen name="Search" component={Search} options={{tabBarVisible: false,}}/>
-      <Tab.Screen name="Chat" component={Chat} options={{tabBarVisible: false,}}/>
-      <Tab.Screen name="About" component={About} options={{tabBarVisible: false,}}/>
+      <Tab.Screen
+        name="Main"
+        component={Main}
+        options={{
+          headerShown: false,
+          tabBarActiveBackgroundColor: "#111",
+          tabBarInactiveBackgroundColor: "#333",
+          tabBarActiveTintColor: "white",
+          //   for adding the styles in the tabBar Text
+          tabBarLabelStyle: { fontSize: 12, fontWeight: "bold" },
+          tabBarIcon : (() => {
+            return <Ionicons name="home-outline" size={20} color="#ffffff" />
+          })
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarVisible: false, //   for adding the styles in the tabBar Text
+          tabBarLabelStyle: { fontSize: 12, fontWeight: "bold" },
+          tabBarActiveBackgroundColor: "#111",
+          tabBarInactiveBackgroundColor: "#333",
+          tabBarActiveTintColor: "white",
+          tabBarIcon : (() => {
+            return <Feather name="search" size={20} color="#ffffff" />
+          })
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={Chat}
+        options={{
+          tabBarVisible: false,
+          //   for adding the styles in the tabBar Text
+          tabBarLabelStyle: { fontSize: 12, fontWeight: "bold" },
+          tabBarActiveBackgroundColor: "#111",
+          tabBarInactiveBackgroundColor: "#333",
+          tabBarActiveTintColor: "white",
+          tabBarIcon : (() => {
+            return <Ionicons name="ios-chatbubble-ellipses-outline" size={20} color="#ffffff" />
+          })
+        }}
+      />
+      <Tab.Screen
+        name="About"
+        component={About}
+        options={{
+          tabBarVisible: false,
+          //   for adding the styles in the tabBar Text
+          tabBarLabelStyle: { fontSize: 12, fontWeight: "bold" },
+          tabBarActiveBackgroundColor: "#111",
+          tabBarInactiveBackgroundColor: "#333",
+          tabBarActiveTintColor: "white",
+          tabBarIcon : (() => {
+            return <Feather name="user" size={20} color="#ffffff" />
+          }) , 
+        //   tabBarStyle : {paddingBottom : 0}
+        }}
+      />
       {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
     </Tab.Navigator>
   );
 }
 
-export default BottomMain
+export default BottomMain;
--- MobileApp/Mobile_Dev/components/Main.js
+++ MobileApp/Mobile_Dev/components/Main.js
@@ -32,7 +32,7 @@ export default function Main() {
         <Carousel/>
         
 
-        <BottomNavigation/>
+        {/* <BottomNavigation/> */}
 
         {/* <LoginUI/> */}
 
